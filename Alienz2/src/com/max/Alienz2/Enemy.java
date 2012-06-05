d000960062be8d7152bbc859b3c6cd71d2afeed0
James <james.waudby@gmail.com>
2012-06-05 15:03:10 +0100

    First Change!

---
+++ Alienz2/src/com/max/Alienz2/Enemy.java
@@ -0,0 +1,12 @@
+package com.max.Alienz2;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Enemy extends Sprite {
+
+	public Enemy() {
+		
+	}
+	
+	
+}
---
+++ Alienz2/src/com/max/Alienz2/Wave.java
@@ -0,0 +1,20 @@
+package com.max.Alienz2;
+
+import com.badlogic.gdx.utils.Array;
+
+public class Wave {
+	
+	Array<Enemy> enemies = new Array<Enemy>();
+	
+	public Wave() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
+	public void update() {
+		
+	}
+}
